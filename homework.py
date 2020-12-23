d117c2d7d99de87f926aacd220e9993db1f95fd5
Кирилл Шубарин <kirill.shubarin@gmail.com>
2020-12-23 18:52:54 +0300

    fix after code-review. Add exceptions and logging

--- homework.py
+++ homework.py
@@ -1,43 +1,84 @@
+import logging
 import os
+import sys
 import time
-import requests
 
+import requests
 from dotenv import load_dotenv
 from twilio.rest import Client
 
 
 load_dotenv()
-account_sid = os.getenv("TWILIO_ACCOUNT_SID")
-auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-client = Client(account_sid, auth_token)
+LOG_FILENAME = "programm.log"
+VK_URL_API = "https://api.vk.com/method/users.get"
+VK_ACCESS_TOKEN = os.getenv("VK_ACCESS_TOKEN")
+VK_API_VERSION = 5.92
+NUMBER_FROM = os.getenv("NUMBER_FROM")
+NUMBER_TO = os.getenv("NUMBER_TO")
+ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+
+logging.basicConfig(level=logging.DEBUG,
+                    filename=LOG_FILENAME,
+                    filemode="a",
+                    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s')
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+
+class HTTPError(Exception):
+    pass
 
 
 def get_status(user_id):
     params = {
         "user_ids": user_id,
         "fields": "online",
-        "v": 5.92,
-        "access_token": os.getenv("VK_ACCESS_TOKEN")
+        "v": VK_API_VERSION,
+        "access_token": VK_ACCESS_TOKEN
     }
-    is_online = requests.post("https://api.vk.com/method/users.get",
-                              params=params).json().get("response")[0].get("online")
-    return is_online
+    is_online = 0  # по-умолчанию считаем, что что-то пошло не так
+    try:
+        response = requests.post(VK_URL_API, params=params)
+        response_json = response.json()
+        # Проверяем что данные пришли
+        if not response_json:
+            raise HTTPError("Проблемы соединения")
+        is_online = response_json.get("response")[0].get("online")
+    except HTTPError as error:
+        logging.error(error, exc_info=True)
+    except KeyError as error:
+        logging.error(error, exc_info=True)
+    except IndexError as error:
+        logging.error(error, exc_info=True)
+    except Exception as error:
+        logging.error(error, exc_info=True)
+    finally:
+        return is_online
 
 
 def sms_sender(sms_text):
     message = client.messages \
         .create(
         body=sms_text,
-        from_=os.getenv("NUMBER_FROM"),
-        to=os.getenv("NUMBER_TO")
+        from_=NUMBER_FROM,
+        to=NUMBER_TO
     )
     return message.sid
 
 
 if __name__ == "__main__":
-    vk_id = input("Введите id ")
-    while True:
-        if get_status(vk_id) == 1:
-            sms_sender(f"{vk_id} сейчас онлайн!")
-            break
-        time.sleep(5)
+    try:
+        vk_id = input("Введите id ")
+        # Проверяем что пользователь ввел только цифры
+        if not all([ch.isdigit() for ch in vk_id]):
+            raise ValueError
+    except ValueError as error:
+        logging.error(error, exc_info=True)
+        sys.exit()
+    else:
+        while True:
+            if get_status(vk_id) == 1:
+                sms_sender(f"{vk_id} сейчас онлайн!")
+                break
+            time.sleep(5)
